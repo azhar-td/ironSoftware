1793e967616465d0d8e946c5157ac023d1dc9dd3
azhar-td <azhar.teradata@gmail.com>
2022-11-23 23:06:05 +0500

    Coding task has been completed.

---
+++ OldPhonePad/OldPhonePad/PadData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OldPhonePad
+{
+    public static class PadData
+    {
+        public static Dictionary<string,char> GetPadData()
+        {
+            Dictionary<string, char> dataDictionary = new Dictionary<string, char>();
+            dataDictionary.Add("1", '&');
+            dataDictionary.Add("11", '\'');
+            dataDictionary.Add("111", '(');
+            dataDictionary.Add("2", 'A');
+            dataDictionary.Add("22", 'B');
+            dataDictionary.Add("222", 'C');
+            dataDictionary.Add("3", 'D');
+            dataDictionary.Add("33", 'E');
+            dataDictionary.Add("333", 'F');
+            dataDictionary.Add("4", 'G');
+            dataDictionary.Add("44", 'H');
+            dataDictionary.Add("444", 'I');
+            dataDictionary.Add("5", 'J');
+            dataDictionary.Add("55", 'K');
+            dataDictionary.Add("555", 'L');
+            dataDictionary.Add("6", 'M');
+            dataDictionary.Add("66", 'N');
+            dataDictionary.Add("666", 'O');
+            dataDictionary.Add("7", 'P');
+            dataDictionary.Add("77", 'Q');
+            dataDictionary.Add("777", 'R');
+            dataDictionary.Add("7777", 'S');
+            dataDictionary.Add("8", 'T');
+            dataDictionary.Add("88", 'U');
+            dataDictionary.Add("888", 'V');
+            dataDictionary.Add("9", 'W');
+            dataDictionary.Add("99", 'X');
+            dataDictionary.Add("999", 'Y');
+            dataDictionary.Add("9999", 'Z');
+            dataDictionary.Add("0", ' ');
+            return dataDictionary;
+        }
+    }
+}
---
+++ OldPhonePad/OldPhonePad/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OldPhonePad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputData = InputData();
+            foreach (string input in inputData)
+            {
+                Console.WriteLine(OldPhonePad(input));
+            }
+        }
+        public static List<string> InputData()
+        {
+            var result = new List<string>();
+            result.Add("33#");
+            result.Add("227*#");
+            result.Add("4433555 555666#");
+            result.Add("8 88777444666*664#");
+            result.Add("6884426 623#");
+            result.Add("6884426 623029999442777#");
+            result.Add("6884426 62302999944277707772333444778833#");
+            result.Add("6884426 62302999944277707772333444778833*33#");
+            return result;
+        }
+        public static string OldPhonePad(string input)
+        {
+            //Get pad data
+            var dataDictionary = PadData.GetPadData();
+            string output = string.Empty;
+            string subString = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                //for # sign
+                if(input[i] == '#' && !string.IsNullOrEmpty(subString))
+                {
+                    output = output + dataDictionary[subString];
+                    return output;
+                }
+                //for backspace
+                if(input[i] == '*' && !string.IsNullOrEmpty(subString))
+                {
+                    subString = string.Empty;
+                    continue;
+                }
+                //for space
+                if(input[i] == ' ' && !string.IsNullOrEmpty(subString))
+                {
+                    output = output + dataDictionary[subString];
+                    subString = string.Empty;
+                    continue;
+                }
+                if (!string.IsNullOrEmpty(subString))
+                {
+                    if (subString[subString.Length - 1] == input[i])
+                        subString = subString + input[i];
+                    else
+                    {
+                        output = output + dataDictionary[subString];
+                        subString = input[i].ToString();
+                    }
+                }
+                else
+                {
+                    subString = subString + input[i];
+                }
+            }
+            return output;
+        }
+    }
+}
